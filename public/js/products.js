e53f881c37050a93741322b408c778dc17b4c31e
inItnd <nadinvladi87@gmail.com>
2018-01-08 13:08:53 +0300

    Мой конфигуратор

---
+++ public/js/products.js
@@ -0,0 +1,106 @@
+const products = [
+  {
+    name: 'Мобильный телефон',
+    type: 'Samsung',
+    category: 'Samsung Galaxy A3 ',
+    color: 'Черный',
+    parameter: 'Разрешение 1280x720',
+    img: '3.jpg',
+  },
+  {
+    name: 'Мобильный телефон',
+    type: 'Samsung',
+    category: 'Samsung Galaxy A5',
+    color: 'Золотой',
+    parameter: 'Диагональ 5.2',
+    img: '2.jpg',
+  },
+  {
+    name: 'Мобильный телефон',
+    type: 'ASUS',
+    category: 'ASUS ZenFone 4 Selfie Pro ',
+    color: 'Красный',
+    parameter: 'Фотокамера 16 мп',
+    img: '4.jpg',
+  },
+  {
+    name: 'Мобильный телефон',
+    type: 'Nokia',
+    category: 'Nokia 6',
+    color: 'Черный',
+    parameter: 'Интернет 4G',
+    img: '5.jpg',
+  },
+  
+{
+    name: 'Мобильный телефон',
+    type: 'Caterpillar',
+    category: 'Caterpillar CAT S30',
+    color: 'Черный',
+    parameter: 'Противоударный' ,
+    img: '1.jpg',
+  },
+ 
+{
+    name: 'Мобильный телефон',
+    type: 'Fly',
+    category: 'Fly FS459 Nimbus 16',
+    color: 'Шампань',
+    parameter: 'Аккумулятор (мАч) 1800',
+    img: '6.jpg',
+  },
+
+{
+    name: 'Мобильный телефон',
+    type: 'LG',
+    category: 'LG G4 H818',
+    color: 'Коричневый',
+    parameter: 'Диктофон',
+    img: '7.jpg',
+  },
+
+{
+    name: 'Мобильный телефон',
+    type: 'HTC ',
+    category: 'HTC One E8 Dual Sim ',
+    color: 'Красный',
+    parameter: 'Поддержка 2-х SIM-карт',
+    img: '8.jpg',
+  },
+  
+{
+    name: 'Мобильный телефон',
+    type: 'Nokia',
+    category: 'NOKIA 8 Dual sim',
+    color: 'Медный',
+    parameter: 'Поддержка 2-х SIM-карт',
+    img: '9.jpg',
+  },
+
+ {
+    name: 'Мобильный телефон',
+    type: 'ASUS',
+    category: 'ASUS ZenFone Zoom ZX551ML ',
+    color: 'Белый',
+    parameter: 'MP3 плеер',
+    img: '10.jpg',
+  },
+  
+{
+    name: 'Мобильный телефон',
+    type: 'Samsung',
+    category: 'SAMSUNG Galaxy J5 2017 SM-J530F',
+    color: 'Голубой',
+    parameter: 'Диагональ 5.2',
+    img: '11.jpg',
+  },
+
+  {
+    name: 'Название 5',
+    type: 'Huawei',
+    category: 'Huawei Honor 8 Lite ',
+    color: 'Синий',
+    parameter: 'Поддержка 2-х SIM-карт',
+    img: 'product.jpg',
+  }
+];
